7233a7043a179e28c819f8629b99d0810df4ac5a
Cassian <cassian@godsted.com>
2026-04-24 12:13:49 -0400

    comment the config

--- src/main/java/cc/cassian/immersiveminimaps/config/ModConfig.java
+++ src/main/java/cc/cassian/immersiveminimaps/config/ModConfig.java
@@ -25,19 +25,28 @@ public class ModConfig extends WrappedConfig {
 
 	@DisplayName("Enable Minimap")
 	public boolean minimap_enable = true;
+	@Comment("NOT YET IMPLEMENTED")
 	public boolean moved_by_effects = true;
 	@Comment("Hide the minimap when the F3 menu is open.")
 	public boolean hide_from_debug = true;
 	public int defaultScale = -1;
 	public int size = 80;
+	@Comment("NOT YET IMPLEMENTED")
 	public boolean left_align = true;
 
 	@Comment({"Options to change the visuals of the map to be more or less vanilla-style."})
 	public Requirements requirements = new Requirements();
 	public static class Requirements implements WrappedConfig.Section {
+		@DisplayName("Require item")
+		@Comment("Require an item to activate overlays. This is the intended way to use the mod.")
 		public boolean require_item = true;
+		@DisplayName("Allow items in bundles/containers")
+		@Comment("As an example, allows for a compass inside a bundle or Shulker Box to count.")
 		public boolean search_containers = true;
+		@DisplayName("Allow containers in bundles/containers")
+		@Comment("As an example, allows for a compass inside a bundle inside a Shulker Box to count.")
 		public boolean search_containers_for_containers = true;
+		@DisplayName("Items that show minimap overlay")
 		public ValueList<String> items = ValueList.create("", "minecraft:map", "minecraft:filled_map");
 	}
 
